14088cd5177cbba8c0bf18df5a533c328746fbc2
xkurozaru <kurozaru.210@gmail.com>
2026-01-02 17:56:43 +0900

    feat: :sparkles: Change id length

--- eroge_review_server/db/schema.sql
+++ eroge_review_server/db/schema.sql
@@ -2,7 +2,7 @@
 -- NOTE: This SQL is written for PostgreSQL.
 
 CREATE TABLE IF NOT EXISTS game_spec (
-    id CHAR(36) PRIMARY KEY,
+    id CHAR(32) PRIMARY KEY,
     title TEXT NOT NULL,
     brand TEXT
 );
